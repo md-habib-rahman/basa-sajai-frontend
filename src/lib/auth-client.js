01b2ec78a42b7e1435f90409ca9b5a50fe96e342
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2026-08-20 13:18:39 +0600

    auth client base url changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,17 @@
+// import { createAuthClient } from "better-auth/react";
+
+// export const authClient = createAuthClient({
+//   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+// });
+
+// export const { useSession, signIn, signOut } = authClient;
+
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL.replace(/\/$/, "")}/auth`
+    : "https://basa-sajai-backend.vercel.app/api/auth",
 });
 
-export const { useSession, signIn, signOut } = authClient;
+export const { signIn, signOut, useSession } = authClient;
